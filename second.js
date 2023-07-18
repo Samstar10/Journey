0bf706bac346f7db8b5ebbffea66620689d84ab6
Samuel Muli <samuelmuli95@gmail.com>
2023-07-18 18:39:35 +0300

    what I have learnt today

---
+++ second.js
@@ -0,0 +1,8 @@
+//Create an array of movies with at least three movies.
+let arrMovies = ['F & F', 'HIMYM', 'Friends'];
+//Using the array from above, store the first movie in a variable
+let firstMovie = arrMovies[0];
+//Get the length of the original array and store it in a new variable
+let arrLength = arrMovies.length;
+//Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+let lastMovie = arrMovies[arrMovies.length - 1];
