2e8d05facb6b9d0158e63f3ce4539eb237205d19
Samuel Muli <samuelmuli95@gmail.com>
2023-07-19 17:35:23 +0300

    19-07-2023

---
+++ 19th-3.js
@@ -0,0 +1,6 @@
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function multNumsArr(arr){
+    let product = 1;
+    arr.forEach((sum) => product *= num);
+    alert(product);
+}
